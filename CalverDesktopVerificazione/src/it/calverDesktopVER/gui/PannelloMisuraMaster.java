2959aeba878fa1fcaee27d52ea87d88e1c4ed067
raffaele.fantini <raffaele.fantini@ID0093.business.local>
2020-08-31 11:10:48 +0200

    commit

--- CalverDesktopVerificazione/src/it/calverDesktopVER/gui/PannelloMisuraMaster.java
+++ CalverDesktopVerificazione/src/it/calverDesktopVER/gui/PannelloMisuraMaster.java
@@ -331,7 +331,7 @@ public class PannelloMisuraMaster extends JPanel
 		}
 
 	//	final JComboBox<String> comboBox_lista_campioni = new JComboBox<String>();
-		final JComboBox<String> comboBox_lista_campioni = new JComboBox<String>(/*listaCampioniCompleta*/);
+		final JComboBox<String> comboBox_lista_campioni = new JComboBox<String>(listaCampioniCompleta);
 		comboBox_lista_campioni.setFont(new Font("Arial", Font.BOLD, 12));
 		pannelloDatiGenerali.add(comboBox_lista_campioni, "cell 2 7");
 
